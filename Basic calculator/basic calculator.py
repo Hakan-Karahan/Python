d8731030e9507193c3fd2145f17bc5a7e723aa1f
Hakan Karahan <147738287+Hakan-Karahan@users.noreply.github.com>
2024-08-26 10:54:42 +0300

    Add files via upload

---
+++ Basic calculator/basic calculator.py
@@ -0,0 +1,48 @@
+import calculator_art
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+
+}
+
+
+def calculator():
+    print(calculator_art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation:")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1,num2) 
+        print(f"{num1}{operation_symbol}{num2} = {answer}") 
+
+        choice = input(f"Type 'y' to continue calculating with {answer},or type 'n' start a new calculation:")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n"*20)
+            calculator()
+
+
+calculator()                
